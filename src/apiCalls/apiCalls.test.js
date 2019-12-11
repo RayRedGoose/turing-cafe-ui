3fcf365e1abeaac015b855dce8309f43aa6d7280
Raisa Primerova <riyuhide@gmail.com>
2019-12-11 11:59:42 -0700

    Add tests for POST request

--- src/apiCalls/apiCalls.test.js
+++ src/apiCalls/apiCalls.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { getReservations } from './apiCalls'
+import { getReservations, postReservation } from './apiCalls'
 
 describe("getReservations", () => {
   let mockReservs = [{
@@ -43,5 +43,59 @@ describe("getReservations", () => {
       return Promise.reject(Error('Fetch is failed'))
     })
     expect(getReservations()).rejects.toEqual(Error('Fetch is failed'))
+  })
+})
+
+describe("postReservation", () => {
+  let mockOptions
+  const mockReserv = {
+    id: 2,
+    name: 'Mark',
+    date: '12/22',
+    time: '5:00',
+    number: '5'
+  }
+
+  beforeEach(() => {
+    mockOptions = {
+      method: 'POST',
+      body: JSON.stringify(mockReserv),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockReserv)
+      })
+    })
+  })
+
+  it("should call fetch with correct url and options", () => {
+    postReservation(mockReserv)
+
+    expect(window.fetch).toHaveBeenCalledWith('http://localhost:3001/api/v1/reservations', mockOptions)
+  })
+
+  it("should return array if fetch is completed correctly", () => {
+    expect(postReservation(mockReserv)).resolves.toEqual(mockReserv)
+  })
+
+  it("should return error if fetch is completed incorrectly", () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: false
+      })
+    })
+    expect(postReservation(mockReserv)).rejects.toEqual(Error('Reservation wasn\'t posted'))
+  })
+
+  it("should return error if fetch is failed", () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.reject(Error('Fetch is failed'))
+    })
+    expect(postReservation(mockReserv)).rejects.toEqual(Error('Fetch is failed'))
   })
 })
